1c68c616a1e9c2be3c5526e7d30820711992172c
Chad Smith <chadxsmith@ymail.com>
2016-01-30 22:34:32 -0500

    login button now stays put when adjusting the screen

--- js/components/sidebarNav.js
+++ js/components/sidebarNav.js
@@ -8,6 +8,22 @@ $( document ).ready(function() {
 		  $(".container").toggleClass('container--expanded')  
 
 	});
+
+	$( window ).resize(function() {
+  		
+
+  		var defaultWidth = 890;
+  		var newWidth = parseInt($(window).width(),10);
+
+  		if (newWidth < 890)
+  			console.log("let's do work!!!!!")
+  			var difference = newWidth - defaultWidth;
+  			console.log(difference)
+  			var loginRight = 20 
+  			var incrementRight = -330 + difference  	
+
+  			$(".login").css("right", incrementRight);
+	});
 	 
 });
 
